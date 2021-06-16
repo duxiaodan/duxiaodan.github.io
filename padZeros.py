56642c61d837aee700628b93a76547745cd41440
Xiaodan Du <duxiaodan@Xiaodans-MacBook-Pro.local>
2021-06-16 10:22:00 -0500

    Added colorized output results

--- padZeros.py
+++ padZeros.py
@@ -2,14 +2,14 @@ import os
 import glob
 from pathlib import Path
 
-# for file_path in Path('./').glob('**/*.jpg'):
-#     # print(file_path)
-#     strpath = str(file_path)
-#     newpath = strpath[:-5]+'00'+strpath[-5:]
-#     os.rename(file_path,newpath)
-
-for file_path in Path('./').glob('**/*.png'):
+for file_path in Path('./images/Output_colorize').glob('**/*.jpg'):
     # print(file_path)
     strpath = str(file_path)
     newpath = strpath[:-5]+'00'+strpath[-5:]
     os.rename(file_path,newpath)
+
+# for file_path in Path('./images/Output_colorize').glob('**/*.png'):
+#     # print(file_path)
+#     strpath = str(file_path)
+#     newpath = strpath[:-5]+'00'+strpath[-5:]
+#     os.rename(file_path,newpath)
