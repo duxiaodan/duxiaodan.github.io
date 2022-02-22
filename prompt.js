04ea3a1b3ec9dd8a5c4e5cac2f282f0d9c531e46
Xiaodan Du <duxiaodan@Xiaodans-MacBook-Pro.local>
2022-02-22 01:22:56 -0600

    Updated text 2 img results of cVAE v7 KNN

---
+++ prompt.js
@@ -0,0 +1,6 @@
+var N = 10;
+var pi = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13'];
+var im_folders = ['threelevels_landmarks_reg_avg2','v7','v7','v7','cVAE_v7','cVAE_v7','cVAE_v7','cVAE_v7','cVAE_v7'];
+var display_name = ['threelevels KNN','v7 Image', 'v7 0 4std', 'v7 KNN','cVAE v7 Image','cVAE v7 0 4std','cVAE v7 0 1std','cVAE v7 0 2std','cVAE v7 KNN'];
+var iter = [515000,153000,153000, 153000, 104000, 104000,104000,104000, 158000]
+var prefix = ['_NN_(50_10)_','_TI_','_Tstd4_','_NN_(50_10)_','_TI_','_Tstd4_','_Tstd1_','_Tstd2_','_NN_(50_10)_']
